ed811aa287435919f8edf2f8e4302647888e6d0f
IreneBioque <irenebioque@gmail.com>
2021-11-09 10:16:38 +0100

    add basic template

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+// Fichero src/components/App.js
+import { useEffect, useState } from 'react';
+import { Link, Route, Switch, useRouteMatch } from 'react-router-dom';
+const App = () => {
+  return <div>¡Hola mundo!</div>;
+};
+
+export default App;
