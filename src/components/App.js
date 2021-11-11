9734b30f1f1a904be56b5deff3f7e6c77e779e83
IreneBioque <irenebioque@gmail.com>
2021-11-11 19:48:27 +0100

    add basic structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,55 @@
-// Fichero src/components/App.js
 import { useEffect, useState } from 'react';
-import { Link, Route, Switch, useRouteMatch } from 'react-router-dom';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import '../styles/App.scss';
+import callToApi from '../services/api';
+import CompanyList from './CompanyList';
+import Header from './Header';
+
 const App = () => {
-  return <div>¡Hola mundo!</div>;
+  const [company, setCompany] = useState([]);
+  const [industry, setIndustry] = useState('all');
+  const [hidden, setHidden] = useState('hidden');
+  const [titleHidden, setTitleHidden] = useState('');
+
+  useEffect(() => {
+    callToApi().then((response) => {
+      setCompany(response);
+    });
+  }, []);
+  const filteredData = company.filter(
+    (data) => industry === 'all' || data.industry === industry
+  );
+  const handleSearchIndustry = (value) => {
+    setIndustry(value);
+  };
+  const handleAll = () => {
+    setIndustry('all');
+  };
+
+  const handleHidden = () => {
+    if (hidden === '') {
+      setHidden('hidden');
+      setTitleHidden('');
+    } else {
+      setHidden('');
+      setTitleHidden('hidden');
+    }
+  };
+
+  return (
+    <div>
+      <Header
+        handleHidden={handleHidden}
+        data={company}
+        handleSearchIndustry={handleSearchIndustry}
+        hidden={hidden}
+        handleAll={handleAll}
+        titleHidden={titleHidden}
+      />
+
+      <CompanyList data={filteredData} />
+    </div>
+  );
 };
 
 export default App;
